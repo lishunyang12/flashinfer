66643c37ca978264bd5748b472458d8eccafad3a
david6666666 <530634352@qq.com>
2026-09-03 06:43:34 +0000

    perf(sm120): uncap pipelined VSA registers

--- flashinfer/cute_dsl/sparse/sm120_blk64/flash_fwd_sm120.py
+++ flashinfer/cute_dsl/sparse/sm120_blk64/flash_fwd_sm120.py
@@ -1283,7 +1283,7 @@ class BlockSparseAttnForwardSm120Blk64(BatchedStaticSchedulerMixin):
                 cluster=(1, 1, 1),
                 smem=self.shared_storage_t.size_in_bytes(),  # type: ignore[attr-defined]
                 stream=stream,
-                min_blocks_per_mp=2,
+                min_blocks_per_mp=1,
             )
         else:
             self.kernel_sequential(
